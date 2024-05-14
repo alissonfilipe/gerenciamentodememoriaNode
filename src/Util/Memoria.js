f444f2fa97bd98770703114450d4ee2efa9184c9
Alisson <alisson.santos4@estudante.ifto.edu.br>
2024-05-14 15:17:59 -0300

    comentando Memoria.js

--- src/Util/Memoria.js
+++ src/Util/Memoria.js
@@ -1,11 +1,13 @@
 class Memoria {
     constructor(id, estado, inicio, tamanho) {
+        // Inicializa os atributos da classe com os valores fornecidos
         this.idProcesso = id;
         this.estado = estado;
         this.inicio = inicio;
         this.tamanho = tamanho;
     }
 
+    // Métodos para obter e definir o ID do processo
     getIdProcesso() {
         return this.idProcesso;
     }
@@ -14,6 +16,7 @@ class Memoria {
         this.idProcesso = id;
     }
 
+    // Métodos para obter e definir o estado da memória
     getEstado() {
         return this.estado;
     }
@@ -22,6 +25,7 @@ class Memoria {
         this.estado = estado;
     }
 
+    // Métodos para obter e definir o início da memória
     getInicio() {
         return this.inicio;
     }
@@ -30,6 +34,7 @@ class Memoria {
         this.inicio = inicio;
     }
 
+    // Métodos para obter e definir o tamanho da memória
     getTamanho() {
         return this.tamanho;
     }
@@ -38,9 +43,10 @@ class Memoria {
         this.tamanho = tamanho;
     }
 
+    // Método para representar o objeto como uma string
     toString() {
         return `(id processo: ${this.idProcesso}), ${this.estado}, ${this.inicio}, ${this.tamanho}`;
     }
 }
 
-module.exports = Memoria;
+module.exports = Memoria; // Exporta a classe Memoria para uso externo
