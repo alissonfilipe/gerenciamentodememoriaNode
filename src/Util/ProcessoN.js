a1df376bbab15e5062d9e6b3cd4c295651f1e1e8
Alisson <alisson.santos4@estudante.ifto.edu.br>
2024-05-14 15:27:36 -0300

    adicionando cometários

--- src/Util/ProcessoN.js
+++ src/Util/ProcessoN.js
@@ -1,9 +1,11 @@
 class ProcessoN {
     constructor(id, tamanho) {
+        // Inicializa os atributos da classe com os valores fornecidos
         this.id = id;
         this.tamanho = tamanho;
     }
 
+    // Métodos para obter e definir o ID do processo
     getId() {
         return this.id;
     }
@@ -12,6 +14,7 @@ class ProcessoN {
         this.id = id;
     }
 
+    // Métodos para obter e definir o tamanho do processo
     getTamanho() {
         return this.tamanho;
     }
@@ -20,9 +23,13 @@ class ProcessoN {
         this.tamanho = tamanho;
     }
 
+    // Método para representar o objeto como uma string
     toString() {
         return `id: ${this.id}, tamanho: ${this.tamanho}`;
     }
 }
 
-module.exports = ProcessoN;
+module.exports = ProcessoN; // Exporta a classe ProcessoN para uso externo
+
+/**Este código define uma classe ProcessoN com construtor e métodos para 
+manipular atributos como o ID e o tamanho do processo, além de um método toString para representar o objeto como uma string formatada. */
