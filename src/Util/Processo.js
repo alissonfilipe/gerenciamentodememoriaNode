0bf36b1befefff69adc4d96ee57ddc0bd0479367
Alisson <alisson.santos4@estudante.ifto.edu.br>
2024-05-14 15:21:47 -0300

    comentando Processo.js

--- src/Util/Processo.js
+++ src/Util/Processo.js
@@ -1,11 +1,13 @@
 class Processo {
     constructor(computacao, alocado, visitado, id) {
+        // Inicializa os atributos da classe com os valores fornecidos
         this.id = id;
         this.computacao = computacao;
         this.alocado = alocado;
         this.visitado = visitado;
     }
 
+    // Métodos para obter e definir o ID do processo
     getId() {
         return this.id;
     }
@@ -14,6 +16,7 @@ class Processo {
         this.id = id;
     }
 
+    // Métodos para obter e definir o tempo de computação do processo
     getComputacao() {
         return this.computacao;
     }
@@ -22,6 +25,7 @@ class Processo {
         this.computacao = computacao;
     }
 
+    // Métodos para obter e definir se o processo está alocado
     getAlocado() {
         return this.alocado;
     }
@@ -30,6 +34,7 @@ class Processo {
         this.alocado = alocado;
     }
 
+    // Métodos para obter e definir se o processo foi visitado
     getVisitado() {
         return this.visitado;
     }
@@ -38,9 +43,12 @@ class Processo {
         this.visitado = visitado;
     }
 
+    // Método para representar o objeto como uma string
     toString() {
         return `Processo [id=${this.id}, computacao=${this.computacao}, alocado=${this.alocado}, visitado=${this.visitado}]`;
     }
 }
 
-module.exports = Processo;
+module.exports = Processo; // Exporta a classe Processo para uso externo
+/*Este código define uma classe Processo com construtor e métodos para manipular atributos como o ID do processo, tempo de computação,
+ se está alocado e se foi visitado, além de um método toString para representar o objeto como uma string formatada. */
